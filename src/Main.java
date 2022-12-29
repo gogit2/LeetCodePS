be5a19afac3cb9e5d6a019c6c97a08dc94def85f
gogit2 <gogit2hd@gmail.com>
2022-12-30 00:21:23 +0200

    Add a repo of solving leet code problems (PS)
    Solve containsDuplicate problem
    use set to store key values when
    triggering duplicate number over
    the true duplicate flag and return true

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int[]nums = {1,2,3,4,2};
+
+        System.out.println(solution.containsDuplicate(nums));
+
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Solution {
+
+    // https://leetcode.com/problems/contains-duplicate/
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> items = new HashSet<>();
+
+        for (int i=0;  i < nums.length; i++) {
+            if (items.contains(nums[i])){
+                return true;
+            }
+            else
+                items.add(nums[i]);
+        }
+
+        return false;
+    }
+
+}
