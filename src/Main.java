1fc2e460ec95709a0fe5c8f1f27926e2eff8acb0
gogit2 <gogit2hd@gmail.com>
2022-12-30 01:29:34 +0200

    Solve valid anagram problem
    convert each string to char array
    and sort them then compare equality

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
+        /*
         int[]nums = {1,2,3,4,2};
-
         System.out.println(solution.containsDuplicate(nums));
+        */
 
+        String name = "car";
+        String noun = "rat";
+        System.out.println(solution.isAnagram(name, noun));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,8 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
 
+    /*
     // https://leetcode.com/problems/contains-duplicate/
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> items = new HashSet<>();
@@ -19,5 +17,21 @@ public class Solution {
 
         return false;
     }
+    */
 
+    // https://leetcode.com/problems/valid-anagram/
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
+        char sChars[] = s.toCharArray();
+        Arrays.sort(sChars);
+        String sortedS = new String(sChars);
+
+        char tChars[] = t.toCharArray();
+        Arrays.sort(tChars);
+        String sortedT = new String(tChars);
+
+        return sortedS.equals(sortedT);
+    }
 }
